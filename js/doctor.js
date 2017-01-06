f94e281155d389cfed90b15b6c893cea1f274a8f
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-06 12:05:45 -0800

    still working on fixing error message

--- js/doctor.js
+++ js/doctor.js
@@ -6,13 +6,17 @@ function Doctors() {
 Doctors.prototype.getDoctors = function(medicalIssue, displayDoctors, displayError) {
   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query="+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(result) {
+      if (result.data.length === 0) {
+        displayError("Please retry and submit another condition");
+      } else {
       result.data.forEach(function(doctor){
         var firstName = doctor.profile.first_name;
         var lastName = doctor.profile.last_name;
         var title = doctor.profile.title;
         displayDoctors(firstName, lastName, title);
         console.log(result);
-      });
+        });
+      }
     })
     .fail(function(error){
       displayError(error.responseJSON.message);
